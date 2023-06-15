eedf0acf49110be7bfc22306a08f3e9519386361
boeyjw-erc-hpc-login2.create.kcl.ac.uk <23478480+boeyjw@users.noreply.github.com>
2023-06-15 23:31:27 +0100

    upd: random sampling with path args

--- src/scripts/dataset/neg_sample_evidence.py
+++ src/scripts/dataset/neg_sample_evidence.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 from tqdm import tqdm
+from pathlib import Path
 
 from common.dataset.reader import JSONLineReader
 from common.util.random import SimpleRandom
@@ -10,9 +11,17 @@ from retrieval.filter_uninformative import uninformative
 
 parser = argparse.ArgumentParser()
 parser.add_argument('db_path', type=str, help='/path/to/fever.db')
+parser.add_argument('datain', type=str, help='/path/to/data-train-dev-test; Filename must have train, test or dev words!')
+parser.add_argument('dataout', type=str, help='/path/to/output')
 
 args = parser.parse_args()
 
+datain = Path(args.datain)
+datain_d = {
+    "train": list(datain.glob("*train*.jsonl")), 
+    "dev": list(datain.glob("*dev*.jsonl")), 
+    "test": list(datain.glob("*test*.jsonl"))
+}
 
 jlr = JSONLineReader()
 
@@ -24,36 +33,38 @@ idx = list(filter(lambda item: not uninformative(item),tqdm(idx)))
 
 r = SimpleRandom.get_instance()
 
-with open("data/fever/test.ns.rand.jsonl", "w+") as f:
-    for line in jlr.read("data/fever-data/test.jsonl"):
-        if line["label"] == "NOT ENOUGH INFO":
+if datain_d["train"]:
+    with open("{0}/{1}.ns.rand.jsonl".format(args.dataout, datain_d["train"][0].stem), "w+") as f:
+        for line in jlr.read(str(datain_d["train"][0])):
+            if line["label"] == "NOT ENOUGH INFO":
 
-            for evidence_group in line['evidence']:
-                for evidence in evidence_group:
-                    evidence[2] = idx[r.next_rand(0, len(idx))]
-                    evidence[3] = -1
-
-
-        f.write(json.dumps(line)+"\n")
-
-with open("data/fever/dev.ns.rand.jsonl", "w+") as f:
-    for line in jlr.read("data/fever-data/dev.jsonl"):
-        if line["label"]=="NOT ENOUGH INFO":
-            for evidence_group in line['evidence']:
-                for evidence in evidence_group:
-                    evidence[2] = idx[r.next_rand(0, len(idx))]
-                    evidence[3] = -1
-
-        f.write(json.dumps(line)+"\n")
+                for evidence_group in line['evidence']:
+                    for evidence in evidence_group:
+                        evidence[2] = idx[r.next_rand(0, len(idx))]
+                        evidence[3] = -1
 
 
+            f.write(json.dumps(line)+"\n")
 
-with open("data/fever/train.ns.rand.jsonl", "w+") as f:
-    for line in jlr.read("data/fever-data/train.jsonl"):
-        if line["label"] == "NOT ENOUGH INFO":
-            for evidence_group in line['evidence']:
-                for evidence in evidence_group:
-                    evidence[2] = idx[r.next_rand(0, len(idx))]
-                    evidence[3] = -1
+if datain_d["dev"]:
+    with open("{0}/{1}.ns.rand.jsonl".format(args.dataout, datain_d["dev"][0].stem), "w+") as f:
+        for line in jlr.read(str(datain_d["dev"][0])):
+            if line["label"]=="NOT ENOUGH INFO":
+                for evidence_group in line['evidence']:
+                    for evidence in evidence_group:
+                        evidence[2] = idx[r.next_rand(0, len(idx))]
+                        evidence[3] = -1
 
-        f.write(json.dumps(line)+"\n")
+            f.write(json.dumps(line)+"\n")
+
+
+if datain_d["test"]:
+    with open("{0}/{1}.ns.rand.jsonl".format(args.dataout, datain_d["test"][0].stem), "w+") as f:
+        for line in jlr.read(str(datain_d["test"][0])):
+            if line["label"] == "NOT ENOUGH INFO":
+                for evidence_group in line['evidence']:
+                    for evidence in evidence_group:
+                        evidence[2] = idx[r.next_rand(0, len(idx))]
+                        evidence[3] = -1
+
+            f.write(json.dumps(line)+"\n")
